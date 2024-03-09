33c780f5798bea80746f634ff2c350b7a4634042
AC <abascharlesbenard@gmail.com>
2024-03-09 20:46:55 +0300

    while loop revision

---
+++ practice/whileloop.dart
@@ -0,0 +1,16 @@
+// Syntax
+// while(condition){
+// statements
+//increment/decrement operation
+//
+// }
+
+// To Print 1 To 10 Using While
+
+void main() {
+  int i = 1;
+  while (i <= 10) {
+    print(i);
+    i++;
+  }
+}
