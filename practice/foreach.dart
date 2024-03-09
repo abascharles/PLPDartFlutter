080d0dd1155a7f220ecd04e8ab3e4a7ab8f62fea
AC <abascharlesbenard@gmail.com>
2024-03-09 18:33:07 +0300

    Incomplete for each to review

---
+++ practice/foreach.dart
@@ -0,0 +1,12 @@
+// Iterates over all lust elements or variables - useful when loo[ping through list/collection
+
+// Syntax
+//  collection.forEach(void f(value));
+
+// Example 1: Print Each Item Of List Using Foreach
+
+void main() {
+  List<String> footballplayers = ['Ronaldo', 'Messi', 'Neymar', 'Hazard'];
+
+  footballplayers.forEach((names) => print(names));
+}
